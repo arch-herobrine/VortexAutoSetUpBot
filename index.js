718ce420fad66f272458731834fd8912eb91484a
arch-herobrine <yuuma9314@outlook.jp>
2023-01-29 12:46:25 +0900

    a

---
+++ index.js
@@ -0,0 +1,67 @@
+const { Client, GatewayIntentBits } = require("discord.js"), dotenv = require("dotenv")
+dotenv.config()
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
+
+client.on("channelCreate", async (ch) => {
+    if (ch.name == "🇰🇵│荒ʖ‘し人民元") {
+        ch.send("ミュートロールセットアップ開始")
+        var chs = JSON.parse(JSON.stringify(ch.guild))
+        await chs.channels.forEach(function (i) {
+            var a = client.channels.cache.get(i)
+            if (a.type == "GuildCategory") {
+                (async () => {
+                    await a.permissionOverwrites.set([
+                        {
+                            id: '1054247482365853776',
+                            allow: [],
+                            deny: ['ADD_REACTIONS', "SEND_MESSAGES", "CREATE_PUBLIC_THREADS", "CREATE_PRIVATE_THREADS", "CONNECT", "SPEAK"],
+                            type: "role"
+                        }
+                    ], '全自動MuteRoleSetUp');
+                })
+            } else if (a.type == "GuildVoice") {
+                (async () => {
+                    await a.permissionOverwrites.set([
+                        {
+                            id: '1054247482365853776',
+                            allow: [],
+                            deny: ['ADD_REACTIONS', "SEND_MESSAGES", "CREATE_PUBLIC_THREADS", "CREATE_PRIVATE_THREADS", "CONNECT", "SPEAK"],
+                            type: "role"
+                        }
+                    ], '全自動MuteRoleSetUp');
+                })
+            } else if (a.type == "GuildStageVoice") {
+                (async () => {
+                    await a.permissionOverwrites.set([
+                        {
+                            id: '1054247482365853776',
+                            allow: [],
+                            deny: ["CONNECT"],
+                            type: "role"
+                        }
+                    ], '全自動MuteRoleSetUp');
+                })
+            } else if (a.type == "GuildText") {
+                (async () => {
+                    await a.permissionOverwrites.set([
+                        {
+                            id: '1054247482365853776',
+                            allow: [],
+                            deny: ['ADD_REACTIONS', "SEND_MESSAGES", "CREATE_PUBLIC_THREADS", "CREATE_PRIVATE_THREADS"],
+                            type: "role"
+                        }
+                    ], '全自動MuteRoleSetUp');
+                })()
+            }
+        })
+        ch.send("ミュートロールセットアップ完了")
+    }
+}
+)
+
+client.on("ready", () => {
+    console.log(`${client.user.tag}でログインしたンゴ`)
+})
+
+client.login(process.env.token)
